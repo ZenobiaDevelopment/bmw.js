07b73bf8d7ab9c0cbdae313376118026e3c9b087
trojanz07 <trojanzogaj223@gmail.com>
2024-01-24 10:06:59 +0100

    Update Client.js

--- src/Client/Client.js
+++ src/Client/Client.js
@@ -1,2 +1,7 @@
 #!/usr/bin/env node --no-warnings --experimental-specifier-resolution=node
 
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
