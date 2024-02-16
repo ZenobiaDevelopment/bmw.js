9baba515a5f92212af806a8a021eef89dac447fa
trojanz07 <trojanzogaj223@gmail.com>
2024-02-16 10:50:11 +0100

    Update Client.js

--- src/Client/Client.js
+++ src/Client/Client.js
@@ -3,8 +3,48 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
 import fetch from 'node-fetch';
+import {parseString} from 'xml2js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Under Development
+let server = 'https://apim.bmwgroup.com/aftersales';
+let token;
+
+class BMW {
+    constructor() {
+        this.data = [];
+    }
+    login(token) {
+        this.token = token;
+    }
+
+    getVehicleBasic(vin, language, callback) {
+        if(!vin) return;
+
+        let hostname;
+
+        if (language) {
+            hostname = server + `/vehicle-identification/vehicle-basic/v1?vin=${vin}&language=${language}`
+        } else if(!language) {
+            hostname = server + `/vehicle-identification/vehicle-basic/v1?vin=${vin}`
+        }
+
+
+        fetch(hostname, {
+            method: 'GET',
+        }).then(res => res.text())
+            .then(data => {
+                parseString(data, (err, result) => {
+                    if (err) {
+                        throw new Error('Error parsing XML data');
+                    }
+                      callback(result);
+                });
+            })
+    }
+
+
+}
+
+export default BMW;
